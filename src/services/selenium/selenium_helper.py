b083b1e45ddadfeb295e89ee3eb950861002c08c
fernando <faguiar408@gmail.com>
2024-10-05 02:21:35 -0300

    feat: added tools for interacting with web pages using Selenium

---
+++ src/services/selenium/selenium_helper.py
@@ -0,0 +1,92 @@
+import os
+import time
+
+from pydantic import UUID4
+from selenium import webdriver
+from selenium.common import ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
+def get_browser_options(state_manager_id: UUID4) -> Options:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless=chrome")  # Executa em modo headless
+    # chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument('--window-size=1920,1080')
+
+    prefs = {
+        "download.default_directory": os.path.join(os.getcwd(), f"temp/{state_manager_id}/"),
+        "download.prompt_for_download": False,
+        "directory_upgrade": True,
+        "safebrowsing.enabled": True,
+        "profile.default_content_setting_values.automatic_downloads": 1
+    }
+
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    return chrome_options
+
+
+def wait(secs: float = 10):
+    time.sleep(secs)
+
+
+class SeleniumHelper:
+
+    def __init__(self, state_manager_id: UUID4):
+        self.driver = webdriver.Chrome(get_browser_options(state_manager_id))
+
+    def quit(self):
+        self.driver.quit()
+
+    def load_page(self, url: str):
+        self.driver.get(url)
+
+    def switch_window(self, tab: int):
+        self.driver.switch_to.window(self.driver.window_handles[tab])
+
+    def close_window(self):
+        self.driver.close()
+
+    def get_html_element(self, element: str) -> str:
+        return self.driver.find_element(by=By.TAG_NAME, value=element).get_attribute("innerHTML")
+
+    @staticmethod
+    def wait(secs: float = 10):
+        time.sleep(secs)
+
+    @staticmethod
+    def wait_for_download_completion(state_manager_id: UUID4):
+        download_dir = os.path.join(os.getcwd(), f"temp/{state_manager_id}")
+        """
+        Aguarda a conclusão do download no diretório especificado.
+        Retorna True se o arquivo foi baixado com sucesso antes do timeout, False caso contrário.
+        """
+        download_in_progress = True
+
+        while download_in_progress:
+            # Verifica os arquivos no diretório de download
+            files = os.listdir(download_dir)
+            # Filtra os arquivos que continuam sendo baixados (arquivos temporários)
+            download_in_progress = any(file.endswith('.crdownload') or file.endswith('.part') for file in files)
+
+            # Se não houver arquivos temporários, o download está completo
+            if not download_in_progress:
+                return True
+
+            # Espera 1 segundo e verifica novamente
+            wait(1)
+
+        # Se o timeout for atingido e o download não tiver sido concluído
+        return False
+#
+# class FormInteractionHelper:
+#
+#     def change_element_style(self, element, style_property, value):
+#         script = f"arguments[0].style.{style_property} = '{value}';"
+#         self.driver.execute_script(script, element)
---
+++ src/services/selenium/web_interaction_helper.py
@@ -0,0 +1,53 @@
+from pydantic.v1 import UUID4
+from selenium.common import ElementNotInteractableException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from src.services.selenium.selenium_helper import SeleniumHelper
+
+
+class WebInteractionHelper(SeleniumHelper):
+
+    def __init__(self, state_manager_id: UUID4):
+        super().__init__(state_manager_id)
+
+    @staticmethod
+    def click(element: WebElement):
+        try:
+            element.click()
+        except ElementNotInteractableException:
+            pass
+
+    def wait_for_element_presence_by_id(self, element_id: str, timeout: float = 10):
+        return WebDriverWait(self.driver, timeout).until(
+            expected_conditions.presence_of_element_located((By.ID, element_id))
+        )
+
+    def wait_for_element_presence_by_xpath(self, xpath: str, timeout: float = 10):
+        return WebDriverWait(self.driver, timeout).until(
+            expected_conditions.presence_of_element_located((By.XPATH, xpath))
+        )
+
+    def wait_for_presence_of_all_elements_by_xpath(self, xpath, timeout: float = 10):
+        return WebDriverWait(self.driver, timeout).until(
+            expected_conditions.presence_of_all_elements_located((By.XPATH, xpath))
+        )
+
+    def click_option(self, parent_element: WebElement, option):
+
+        self.click(parent_element)
+
+        value = option["value"] if option["value"] else option["label"]
+        option_element = self.wait_for_element_presence_by_xpath(
+            f".//*[contains(@class, '{option["class"]}') and contains(., '{value}')]"
+        )
+
+        self.click(option_element)
+
+    def remove_css_classes(self, element: WebElement):
+        self.driver.execute_script("arguments[0].className = '';", element)
+
+    def remove_inline_styles(self, element: WebElement):
+        self.driver.execute_script("arguments[0].removeAttribute('style');", element)
